3b67149d1590f5c4297938aa75bdf84b5accc029
hase111111 <hasehasehase61@gmail.com>
2024-12-10 21:06:09 +0900

    CMakeFileの追加

---
+++ build/src/CMakeFiles/fishc.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fishc.dir/main.cpp.o"
+  "fishc"
+  "fishc.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fishc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        // Print usage
+        std::cout << "Usage: " << argv[0] << " <input_file>" << std::endl;
+    } else {
+        // Read input file
+        std::string input_file = argv[1];
+        std::cout << "Reading input file: " << input_file << std::endl;
+    }
+
     return 0;
 }
