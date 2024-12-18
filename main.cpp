f9cd6f6e8490d5714d7033ba46980a227ba9b4bc
hase111111 <hasehasehase61@gmail.com>
2024-12-18 18:41:27 +0900

    引数解析のバグの修正

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 
+#include <iostream>
 #include <string>
 
 #include "argument_help_printer.h"
 #include "argument_initializer.h"
 #include "argument_parser.h"
+#include "file_loader.h"
 #include "interpreter.h"
 #include "version.h"
 
@@ -31,12 +33,19 @@ int main(int argc, char **argv) {
     }
 
     // If the user provided a script file, read the file and execute the code.
-    std::string code = "'Hello, World!' rooooo oo oooooo ao;";
+    std::string code = "";
 
     if (arg_parser.HasOption("--code")) {
         code = arg_parser.GetOptionValue<std::string>("--code");
     } else {
         std::string path = arg_parser.GetOptionValue<std::string>("script");
+        fishc::FileLoader file_loader{path};
+        if (!file_loader.IsOpen()) {
+            std::cerr << "Failed to open the file: " << path << std::endl;
+            return 1;   
+        }
+
+        code = file_loader.GetFile();
     }
 
     fishc::Interpreter interpreter(code);
--- src/argument_parser.cpp
+++ src/argument_parser.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 
 #include "argument.h"
+#include "utils.h"
 
 namespace fishc {
 
@@ -38,24 +39,36 @@ bool ArgumentParser::Parse(int argc, char **argv) noexcept {
     const int arg_settings_size = static_cast<int>(argument_settings_.size());
 
     for (int i = 1; i < static_cast<int>(args.size()); ++i) {
+        bool ok = false;
+
         for (int j = 0; j < arg_settings_size; ++j) {
+            if (ok) { break; }
+
             for (const auto &opt : argument_settings_[j].names) {
-                if (args[i] != opt) { 
+                if (ok) { break; }
+
+                if (!utils::IsOption(args[i])) {
                     for (int j = 0; j < arg_settings_size; ++j) {
                         if (!argument_settings_[j].is_option 
                             && !parsed_args_[j].first) {
                             parsed_args_[j].first = true;
                             parsed_args_[j].second = args[i];
+
+                            ok = true;
                             break;
                         }
                     }
-                } else {
-                    parsed_args_[j].first = true;
-                    if (argument_settings_[j].need_argument) {
-                        parsed_args_[j].second = args[i + 1];
-                        ++j;
-                    }
                 }
+                
+                if (args[i] != opt) { continue; }
+
+                parsed_args_[j].first = true;
+                if (argument_settings_[j].need_argument) {
+                    parsed_args_[j].second = args[i + 1];
+                    ++i;
+                }
+
+                ok = true;
             }  // for names
         }  // for argument_settings_
     }  // for args
