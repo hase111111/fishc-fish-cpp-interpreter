718887ea7086d3869b525c45ac89bcebda18b539
hase111111 <hasehasehase61@gmail.com>
2024-12-19 15:58:36 +0900

    CodeBoxのテストの追加

--- include/type.h
+++ include/type.h
@@ -14,6 +14,30 @@ using Number = std::variant<ImplInt, ImplFloat>;
 using Stack = std::deque<Number>;
 using Register = std::optional<Number>;
 
+inline bool IsInt(const Number& n) noexcept {
+    return n.index() == 0;
+}
+
+inline bool IsFloat(const Number& n) noexcept {
+    return n.index() == 1;
+}
+
+[[nodiscard]] inline ImplInt GetIntOr(
+    const Number& n, const ImplInt default_v) noexcept {
+    if (IsInt(n)) {
+        return std::get<ImplInt>(n);
+    }
+    return default_v;
+}
+
+[[nodiscard]] inline ImplFloat GetFloatOr(
+    const Number& n, const ImplFloat default_v) noexcept {
+    if (IsFloat(n)) {
+        return std::get<ImplFloat>(n);
+    }
+    return default_v;
+}
+
 [[nodiscard]] Number operator+(const Number& a, const Number& b) noexcept;
 
 [[nodiscard]] Number operator-(const Number& a, const Number& b) noexcept;
