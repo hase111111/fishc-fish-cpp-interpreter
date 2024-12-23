025669ead238a770847f85272f24e518aff6d7ce
hase111111 <hasehasehase61@gmail.com>
2024-12-23 13:58:22 +0900

    未使用の関数を削除

--- include/utils.h
+++ include/utils.h
@@ -7,7 +7,6 @@
 #define FISHC_UTILS_H_
 
 #include <cassert>
-#include <stdexcept>
 #include <string>
 #include <sstream>
 #include <vector>
@@ -17,26 +16,10 @@ namespace fishc::utils {
 //! @brief Check if the given argument is an option, like "-h" or "--help".
 //! @param arg The argument to check.
 //! @return True if the argument is an option, false otherwise.
-//! @throw std::invalid_argument If arg is empty.
-inline bool IsOption(const std::string &arg) {
-    if (arg.empty()) {
-        throw std::invalid_argument("arg is empty");
-    }
-
+[[nodiscard]]
+inline bool IsOption(const std::string &arg) noexcept {
+    assert(!arg.empty() && "arg must not be empty");
     return arg[0] == '-';
-}
-
-//! @brief Check if the given argument is a one-character option, like "-h".
-//! @param arg The argument to check.
-//! @return True if the argument is a one-character option, false otherwise.
-//! @throw std::invalid_argument If arg is nullptr.
-constexpr bool IsOneCharOption(const char *arg) {
-    if (arg == nullptr) {
-        throw std::invalid_argument("arg is nullptr");
-    }
-
-    // size == 2 and arg[0] == - and arg[1] != -
-    return arg[0] == '-' && arg[1] != '-' && arg[2] == '\0';
 }
 
 template<typename T>
